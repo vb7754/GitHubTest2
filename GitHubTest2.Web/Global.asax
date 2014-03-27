0001e3a7b2adbfc07da79d915d3fbc61a22905eb
vb7754 <vb7754@gmail.com>
2014-03-27 10:33:57 -0500

    inittial

---
+++ GitHubTest2.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitHubTest2.Web.MvcApplication" Language="C#" %>
---
+++ GitHubTest2.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubTest2.Web.Startup))]
+namespace GitHubTest2.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
